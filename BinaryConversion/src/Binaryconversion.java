642fe59d531da82c0b6e8c71e527ca92175aa53b
saritha <sarithaveluru@hotmail.com>
2015-05-30 11:37:14 -0600

    Initial commit - BinnaryConversion project
    this is a first project it does a binnary conversion
    crs-fixed
    author: saritha
    signed -by: mansi

---
+++ BinaryConversion/src/Binaryconversion.java
@@ -0,0 +1,68 @@
+
+
+import java.util.Scanner;
+
+public class Binaryconversion 
+{
+	private static Scanner kbd=new Scanner (System.in);
+	public static void main(String[]args)
+	{
+		
+		int numvalue=0;
+		System.out.println("please enter an integer between 255 and 0");
+		
+		numvalue=kbd.nextInt();
+		
+		if(numvalue>255 || numvalue<0)
+			
+		{ System.out.println("Error: Integer is too high or too low)");
+		
+		}
+		
+		getBinary(numvalue);
+	}
+	public static void getBinary(int numvalue)
+		{
+			String s=""; 
+			for(int j=0; j<8; j++)
+			{
+				if(numvalue % 2==1)
+				{	
+				 s= '1' +s;
+				}
+				if (numvalue %2==0)
+				{
+					s='0' + s;
+				}
+				numvalue=numvalue/2;	
+				System.out.println("Binary equivalent is:" +s);
+			}
+			System.out.println("Binary equivalent is:" +s);
+				
+				
+					
+			
+			}			
+		
+
+		}      
+		
+	
+			
+			
+		
+		
+			
+		
+			
+		
+			
+	
+	
+	
+
+	
+	
+	
+	
+
